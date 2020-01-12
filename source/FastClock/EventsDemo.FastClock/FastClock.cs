70faed884f55aa056f06b6d4955c9b1f21e7566a
Rene Lochner <school@renelochner.at>
2020-01-12 18:56:51 +0100

    Implement "FastClock" class

--- source/FastClock/EventsDemo.FastClock/FastClock.cs
+++ source/FastClock/EventsDemo.FastClock/FastClock.cs
@@ -1,9 +1,44 @@
 ﻿using System;
 using System.Windows.Threading;
 
-namespace EventsDemo.FastClock
-{
-    public class FastClock
-    {
+namespace EventsDemo.Logic {
+    public class FastClock {
+        private static FastClock instance = null;
+
+        private readonly DispatcherTimer _timer;
+        private bool _isRunning;
+
+        public static FastClock GetInstance(DateTime currentDateTime)
+        {
+            if (instance == null)
+            {
+                instance = new FastClock(currentDateTime);
+            }
+            return instance;
+        }
+
+        public DateTime CurrentDateTime { get; private set; }
+
+        public bool IsRunning {
+            get { return _isRunning; }
+            set {
+                _isRunning = value;
+
+                if (_isRunning)
+                {
+                    _timer.Start();
+                }
+                else
+                {
+                    _timer.Stop();
+                }
+            }
+        }
+
+        private FastClock(DateTime currentDateTime)
+        {
+            this._timer = new DispatcherTimer();
+            this.CurrentDateTime = currentDateTime;
+        }
     }
 }
