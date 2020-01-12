efc640dd92c812320d260197c9e7e7622391651d
Rene Lochner <school@renelochner.at>
2020-01-12 18:59:24 +0100

    Add time parsing and handling

--- source/FastClock/EventsDemo.FastClockWpf/MainWindow.xaml.cs
+++ source/FastClock/EventsDemo.FastClockWpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using EventsDemo.Logic;
 
 namespace EventsDemo.FastClockWpf
 {
@@ -8,6 +9,8 @@ namespace EventsDemo.FastClockWpf
     /// </summary>
     public partial class MainWindow
     {
+        private DateTime _dateTime;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -15,24 +18,58 @@ namespace EventsDemo.FastClockWpf
 
         private void MetroWindow_Initialized(object sender, EventArgs e)
         {
+            _dateTime = GetCurrentTime();
+
             DatePickerDate.SelectedDate = DateTime.Today;
-            TextBoxTime.Text = DateTime.Now.ToShortTimeString();
+            TextBoxTime.Text = _dateTime.ToShortTimeString();
+            FastClock.GetInstance(_dateTime);
         }
 
         private void ButtonSetTime_Click(object sender, RoutedEventArgs e)
         {
+            SetFastClockStartDateAndTime();
         }
 
         private void SetFastClockStartDateAndTime()
         {
+            string date = DatePickerDate.Text;
+            string time = TextBoxTime.Text;
+            _dateTime = StringToDateTime(date, time);
+            FastClock.GetInstance(_dateTime);
+        }
+
+        private DateTime StringToDateTime(string dateString, string timeString)
+        {
+            string[] dateParts = dateString.Split('.');
+            string[] timeParts = timeString.Split(':');
+            int day = 0;
+            int month = 0;
+            int year = 0;
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+
+            Int32.TryParse(dateParts[0], out day);
+            Int32.TryParse(dateParts[1], out month);
+            Int32.TryParse(dateParts[2], out year);
+            Int32.TryParse(timeParts[0], out hour);
+            Int32.TryParse(timeParts[1], out minute);
+            if(timeParts.Length >= 3)
+            {
+                Int32.TryParse(timeParts[2], out second);
+            }
+            return new DateTime(year, month, day, hour, minute, second);
         }
 
         private void FastClockOneMinuteIsOver(object sender, DateTime fastClockTime)
         {
+
         }
 
         private void CheckBoxClockRuns_Click(object sender, RoutedEventArgs e)
         {
+            FastClock fastClock = FastClock.GetInstance(_dateTime);
+            fastClock.IsRunning = !fastClock.IsRunning;
         }
 
         private void ButtonCreateView_Click(object sender, RoutedEventArgs e)
@@ -40,6 +77,11 @@ namespace EventsDemo.FastClockWpf
             DigitalClock digitalClock = new DigitalClock();
             digitalClock.Owner = this;
             digitalClock.Show();
+        }
+
+        private DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
         }
     }
 }
